79aa5be0ef49d5d21eee0a36c22c478576b94e0b
Moheeb Zara <mzara1@asu.edu>
2019-01-26 08:24:18 -0700

    Create MKRGSM_MQTT_PUB_SUB.ino

---
+++ MKRGSM_MQTT_PUB_SUB/MKRGSM_MQTT_PUB_SUB.ino
@@ -0,0 +1,141 @@
+/*
+  Adapted from ArduinoMqttClient - WiFi Simple Receive Callback
+
+  This example connects to a MQTT broker and subscribes to a single topic.
+  When a message is received it prints the message to the serial monitor,
+  it uses the callback functionality of the library.
+
+  This example also publishes a sensor value to a separate topic
+
+  The circuit:
+  - Arduino MKR 1000, MKR 1010 or Uno WiFi Rev.2 board
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoMqttClient.h>
+#include <MKRGSM.h>
+
+#include "arduino_secrets.h" 
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+// PIN Number
+const char PINNUMBER[]     = SECRET_PINNUMBER;
+// APN data
+const char GPRS_APN[]      = SECRET_GPRS_APN;
+const char GPRS_LOGIN[]    = SECRET_GPRS_LOGIN;
+const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
+
+// To connect with SSL/TLS:
+// 1) Change GSMClient to GSMSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+GPRS gprs;
+GSM gsmAccess;
+GSMSSLClient gsmClient;
+MqttClient mqttClient(gsmClient);
+
+const char broker[] = "some.secure.server";
+int        port     = 8883;
+const char topic[]  = "hologram/to";
+const char publishTopic[]  = "hologram/from";
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to GSM and GPRS:
+  Serial.print("Attempting to connect to GSM and GPRS");
+   // connection state
+  bool connected = false;
+
+  // After starting the modem with GSM.begin()
+  // attach the shield to the GPRS network with the APN, login and password
+  while (!connected) {
+    if ((gsmAccess.begin(PINNUMBER) == GSM_READY) &&
+        (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY)) {
+      connected = true;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  // set the message receive callback
+  mqttClient.onMessage(onMqttMessage);
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe(topic);
+
+  // topics can be unsubscribed using:
+  // mqttClient.unsubscribe(topic);
+
+  Serial.print("Waiting for messages on topic: ");
+  Serial.println(topic);
+  Serial.println();
+}
+
+void loop() {
+  // call poll() regularly to allow the library to receive MQTT messages and
+  // send MQTT keep alives which avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // Set the rate at which we send our values
+  delay(500);
+  
+  // read the first Analog pin
+  int sensorVal = analogRead(0);
+  
+  // Publish our sensor value
+  mqttClient.beginMessage(publishTopic);
+  mqttClient.print(sensorVal);
+  mqttClient.endMessage();
+}
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.println("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // use the Stream interface to print the contents
+  while (mqttClient.available()) {
+    Serial.print((char)mqttClient.read());
+  }
+  Serial.println();
+
+  Serial.println();
+}
